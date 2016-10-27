40bd557137a2faa5e20ad6b0c1b539920b50d4f4
Madiha_Zahoor <dev.madiha@gmail.com>
2016-10-27 15:03:50 +0500

    progress bar changes2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -147,7 +147,7 @@
                     <div class="col-sm-12">
                         
                         <div class="progress">
-                            <div class="progress-bar" role="progressbar" aria-valuenow="70"
+                            <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="70"
                                  aria-valuemin="0" aria-valuemax="100" style="width:70%">
                                 70%
                             </div>
